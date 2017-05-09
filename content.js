d3f17be2a28af8752d5d6b51dc9e45eab0187426
Andres Mora Vanegas <andymora1907@hotmail.com>
2017-05-09 12:05:14 -0500

    add files to create a chrome extension.

---
+++ content.js
@@ -0,0 +1,6 @@
+var first = document.getElementsByClassName("schema-nav");
+var second = document.getElementsByClassName("col-md-9");
+
+first[0].className += " left";
+second[0].className += " right";
+$("head").append('<meta name="viewport" content="width=device-width, initial-scale=1">');
